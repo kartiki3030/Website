989dbaeb8ddb4c916ff12d1fb36d68ce3fac9b12
kartiki3030 <105437683+kartiki3030@users.noreply.github.com>
2023-10-23 18:36:58 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [
+      'src/components/LoginForm.js',
+      'src/App.js',
+      'src/App.test.js',
+      'src/index.js',
+      'src/reportWebVitals.js',
+      'src/setupTests.js',
+      './public/index.html',
+    ],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+  
